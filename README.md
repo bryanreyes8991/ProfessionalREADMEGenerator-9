2c51a020774db8407bc53c5f6b102777de5379b6
Nebula Rider <bryanreyes8991@gmail.com>
2023-11-10 02:15:33 -0600

    initial inquirer build

---
+++ README.md
@@ -0,0 +1,43 @@
+# Professional README Generator
+
+### Table of Contents
+* [description](#description) Description
+* [installation](#installation) Installation
+* [usage](#usage) Usage
+* [license](#license) License
+* [contributions](#contributions) Contributions
+* [tests](#tests) Tests
+* [questions](#questions) Questions
+
+## Description
+This is a command line application that takes prompts to help developers build a professional README on the fly.
+
+## Installation
+Clone the repository to a local repository.
+
+## Usage
+Invoke the script in index.js with node to receive the prompts to detail your program/software/project/application.
+## License
+ISC
+Copyright (c)
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+PERFORMANCE OF THIS SOFTWARE.
+
+## Contributions
+Open source github contribution
+
+## Tests
+Currently Testing.
+
+## Questions
+For any additional questions contact at https://github.com/bryanreyes8991 or bryanreyes8991@gmail.com
--- index.js
+++ index.js
@@ -1,13 +1,106 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const { writeFile } = require('fs');
+const generateREADME = ({ project, description, usage, license, contribution, installation, test, github, email }) => `
+# ${project}
 
-// TODO: Create an array of questions for user input
-const questions = [];
+### Table of Contents
+* [description](#description) Description
+* [installation](#installation) Installation
+* [usage](#usage) Usage
+* [license](#license) License
+* [contributions](#contributions) Contributions
+* [tests](#tests) Tests
+* [questions](#questions) Questions
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+## Description
+${description}
 
-// TODO: Create a function to initialize app
-function init() {}
+## Installation
+${installation}
 
-// Function call to initialize app
+## Usage
+${usage}
+## License
+${license}
+Copyright (c)
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+PERFORMANCE OF THIS SOFTWARE.
+
+## Contributions
+${contribution}
+
+## Tests
+${test}
+
+## Questions
+For any additional questions contact at https://github.com/${github} or ${email}
+
+`;
+
+const promptUser = () => {
+return inquirer.prompt([
+    {
+        type: 'input',
+        name: 'project',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Describe your project.',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How do you install this project?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use this application?',
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: 'Enter Project License',
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'How does somebody contribute to this project?',
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Enter any testing information',
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your github username',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address.'
+    },
+ ]);
+};
+const init = () => {
+    promptUser()
+    .then((answers) => writeFile('README.md', generateREADME(answers)))
+    .then(() => console.log('Created README!'))
+    .catch((err) => console.error(err));
+};
+
 init();
